cd3608372e3dee34703f12ddf83a7d7ede65bc2d
Matias <matiasruizr@hotmail.es>
2018-11-19 00:35:18 -0400

    Vistas cargadas, inicio maquetado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'index/index'
   resources :regions
   resources :provincia
   resources :comunas
   resources :usuarios
+
+  root :to => 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
